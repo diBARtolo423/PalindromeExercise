986e208b82d56ea542d77320a4f9b8e1e0c8f474
Ross DiBartolo <dibartoloross@gmail.com>
2023-01-23 13:33:14 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+
+        }
+        public bool IsAPalindrome(string word)
+        {
+            if (word.SequenceEqual(word.Reverse()))
+                return true;
+            else
+                return false;
+        }
+
+        //public bool IsAPalindrome(string word)
+        //{
+        //    var reversed = "";
+        //    for(var i = word.Length - 1; i >= 0; i--)
+        //    {
+        //        reversed += word.ToLower()[i];
+        //    }
+        //    if (reversed == word.ToLower())
+        //    {
+        //        return true;
+        //    }
+        //    else { return false; }
+        //}
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,47 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("deed", true)]
+        [InlineData("noon", true)]
+        [InlineData("football", false)]
+        [InlineData("shoes", false)]
+        [InlineData("name", false)]
+        [InlineData("423", false)]
+        public void PalidromeTester(string word, bool expected)
         {
+            var challenger = new WordSmith();
 
+            var actual = challenger.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
+
+        //[Theory]
+        //[InlineData("racecar", true)]
+        //[InlineData("kayak", true)]
+        //[InlineData("deed", true)]
+        //[InlineData("noon", true)]
+        //[InlineData("football", false)]
+        //[InlineData("shoes", false)]
+        //[InlineData("name", false)]
+        //[InlineData("423", false)]
+        //public void PalidromeTester(string word, bool expected)
+        //{
+        //    var challenger = new WordSmith();
+
+        //    var actual = challenger.IsAPalindrome(word);
+
+        //    Assert.Equal(expected, actual);
+        //}
     }
 }
